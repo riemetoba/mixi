8f36a096191d938b3db40793af58400bbfa319b4
riemetoba <riemehasan06@gmail.com>
2025-09-16 23:17:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+// MixitUp Start ==
+ var containerEl = document.querySelector('.myContainer');
+
+            var mixer = mixitup(containerEl);
+// MixitUp End == 
+
+// Sliders Start ==
+
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false
+})
+// Sliders End == 
+
+// MixitUp Start ==
+
+
+// MixitUp End == 
+
+// MixitUp Start ==
+
+// MixitUp End == 
+
+// MixitUp Start ==
+
+// MixitUp End == 
